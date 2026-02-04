cc7a235e792433c0f8217cf5c6de0a99a53de804
Lazy-Panda78 <yash.upadhyay_cs.aiml24@gla.ac.in>
2026-02-04 13:40:05 +0530

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <Arduino.h>
+#include <Servo.h>
+Servo myservo;
+
+int pos = 0;
 void setup() {
+    Serial.begin(9600);
+    myservo.attach(9);
+    Serial.println("Servo initiate");
+
 // write your initialization code here
 }
 
